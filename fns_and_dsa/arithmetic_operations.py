f87447a8ee347c52bb1e011a4bc9e31d039191b9
Philipos Hailu <hailuphilipos@gmail.com>
2024-09-28 14:33:21 -0300

    update 2

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,6 @@
 
 
-def perform_operation(num1, num2, operation):
+def perform_operation(num1 : float, num2 :float, operation : str):
     match operation:
         case 'add':
             answer = num1 + num2
@@ -9,9 +9,9 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             answer = num1 * num2
         case 'divide':
-            if num2 == 0:
-                answer = f"Error, {num1} can not be divided by {num2}."
-            else:
+            if num2 != 0:
                 answer = num1 / num2
-    
-    return anser
+            elif num2 == 0:
+                answer = f"Error, {num1} can not be divided by {num2}."
+    return answer
+
