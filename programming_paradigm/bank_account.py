6471758435a50e51ced151fb2d91dc9715eaecfe
Philipos Hailu <hailuphilipos@gmail.com>
2024-10-06 11:38:37 -0300

    update2

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,6 +14,6 @@ class BankAccount:
 
 
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${floar(self.account_balance)}")
 
 
