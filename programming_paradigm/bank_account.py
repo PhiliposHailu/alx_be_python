589f6fe5911073a22ff212292ea3103198ba89ec
Philipos Hailu <hailuphilipos@gmail.com>
2024-10-06 11:28:02 -0300

    created a bank_account.py file

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,19 @@
+class BankAccount():
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance +=  amount
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+
+
