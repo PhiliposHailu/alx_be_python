4bf269e76fbaf94f8b5f736a3b3be98695294139
Philipos Hailu <hailuphilipos@gmail.com>
2024-10-12 15:39:09 -0300

    update

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,18 +1,15 @@
-from book_class import Book
 
-def main():
-    # Creating an instance of Book
-    my_book = Book("1984", "George Orwell", 1949)
+class Book:
+    def __init__(self, title : str, author : str, year : int) -> None:
+        self.title = title
+        self.author = author
+        self.year = year
 
-    # Demonstrating the __str__ method
-    print(my_book)  # Expected to use __str__
-
-    # Demonstrating the __repr__ method
-    print(repr(my_book))  # Expected to use __repr__
-
-    # Deleting a book instance to trigger __del__
-    del my_book
-
-if __name__ == "__main__":
-    main()
-
+    def __del__(self):
+        print(f"Deleting {self.title}")
+    
+    def __str__(self) -> str:
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
+    def __repr__(self) -> str:
+        return f"Book('{self.title}', '{self.author}', {self.year})"
