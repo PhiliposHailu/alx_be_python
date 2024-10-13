52117d96021b511517f92554f2391787af14aa12
Philipos Hailu <hailuphilipos@gmail.com>
2024-10-13 05:17:49 -0300

    update1 to library_system.py file

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,16 +3,25 @@ class Book:
     def __init__(self, title : str, author : str) -> None:
         self.title = title
         self.author = author
-
+        
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+             
 class EBook(Book):
     def __init__(self, title: str, author: str, file_size : int) -> None:
         super().__init__(title, author)
         self.file_size = file_size
 
+    def __str__(self) -> str:
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count : int) -> None:
         super().__init__(title, author)
         self.page_count = page_count
+
+    def __str__(self) -> str:
+        return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self) -> None:
@@ -23,11 +32,5 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            
-            if isinstance(book, EBook):
-                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
-            elif isinstance(book, Book):
-                print(f"Book: {book.title} by {book.author}")
+            print(book)
 
